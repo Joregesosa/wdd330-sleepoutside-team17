cadb1e6acde202adf35d7e12f35fe0f5cbaba07c
Jorge Sosa <joregesosa@gmail.com>
2025-09-13 11:50:29 -0400

    Add ProductDetails class and product page for displaying product information

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,47 @@
+import { setLocalStorage } from "./utils.mjs";
+ 
+export default class ProductDetails {
+    constructor(product_id, dataSource) {
+        this.product_id = product_id;
+        this.dataSource = dataSource;
+        this.product = {};
+    }
+    async init() {
+        this.product = await this.dataSource.findProductById(this.product_id);
+        this.renderProductDetails();
+        document.getElementById('addToCart')
+            .addEventListener('click', this.addProductToCart.bind(this));
+    }
+
+    addProductToCart() {
+        const product = this.product;
+        const cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+        setLocalStorage("so-cart", [...cart, product]);
+    }
+
+    renderProductDetails() {
+
+        const template = `
+            <h3>${this.product.Brand.Name}</h3>
+
+            <h2 class="divider">${this.product.Name}</h2>
+
+            <img class="divider"
+                src="${this.product.Image}"
+                alt="${this.product.Name}" />
+
+            <p class="product-card__price">$${this.product.FinalPrice}</p>
+
+            <p class="product__color">${this.product.Colors.map(color => color.ColorName).join(", ")}</p>
+
+            <p class="product__description">
+                ${this.product.DescriptionHtmlSimple}
+            </p>
+
+            <div class="product-detail__add">
+                <button id="addToCart" data-id="${this.product.ID}">Add to Cart</button>
+            </div>
+      `;
+        document.querySelector('.product-detail').innerHTML = template;
+    }
+}
