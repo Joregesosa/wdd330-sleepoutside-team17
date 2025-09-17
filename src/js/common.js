82b923b58b1b5263c613325d70e71dc1d06f8c29
Jorge Sosa <joregesosa@gmail.com>
2025-09-17 00:39:35 -0400

    Remove console log from updateCartCount function

--- src/js/common.js
+++ src/js/common.js
@@ -19,7 +19,7 @@ function updateCartCount() {
     cartCount?.remove();
     const cartItems = getLocalStorage('so-cart') || [];
     const itemCount = cartItems.length;
-    console.log(itemCount);
+
     if (itemCount > 0) {
         const template = `<span class="cart__count">${itemCount}</span>`;
         cart.insertAdjacentHTML('beforeend', template);
